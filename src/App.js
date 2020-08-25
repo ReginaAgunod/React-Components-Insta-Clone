4f2f14fbb8ab2855a5e67823034fd5ddc92a68b5
Restyled.io <commits@restyled.io>
2020-08-25 19:49:38 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@
 */
 
 // Import the state hook
-import './App.css';
+import "./App.css";
 
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 // Import the Posts (plural!) and SearchBar components, since they are used
 // inside App component
 // Posts
-import Posts from './components/Posts/Posts';
+import Posts from "./components/Posts/Posts";
 // Search Bar
-import SearchBar from './components/SearchBar/SearchBar';
+import SearchBar from "./components/SearchBar/SearchBar";
 // dummyData
-import dummyData from './dummy-data';
+import dummyData from "./dummy-data";
 
 // checking console for dummyData
 console.log(dummyData);
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  const [posts, setPosts] = useState (dummyData);
-  
+  const [posts, setPosts] = useState(dummyData);
+
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -40,19 +40,18 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts (
-      posts.map ( post => {
+    setPosts(
+      posts.map((post) => {
         if (post.id === postId) {
-          return {...post, like: (post.likes + 1)}
+          return { ...post, like: post.likes + 1 };
         }
-        return post
-      }
-    )
-    )
+        return post;
+      })
+    );
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
       <Posts />
       <SearchBar />
